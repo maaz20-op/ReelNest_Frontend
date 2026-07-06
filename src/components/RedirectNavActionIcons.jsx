ae8ed4091d59f58572440f4a0d06dfd7c430b8f1
Maaz Jatt <maazcoding54@gmail.com>
2026-07-06 21:31:26 +0500

    file added

--- src/components/RedirectNavActionIcons.jsx
+++ src/components/RedirectNavActionIcons.jsx
@@ -16,14 +16,14 @@ export const Nav_Actions = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-full h-15  bg-(--bg-primary)   gap-4 px-2 flex justify-between items-center absolute lg:relative  bottom-0 lg:flex-col lg:h-full lg:border-l lg:py-30 lg:min-h-0 lg:border-(--border-color)  lg:justify-start">
+    <div className="w-full   h-15  bg-(--bg-primary)   gap-4 px-2 flex justify-between items-center absolute lg:relative  bottom-0 lg:flex-col lg:h-full lg:border-l lg:py-30 lg:min-h-0 lg:border-(--border-color)  lg:justify-start">
       {IconsContent.map(({ text, icon: Icon, redirect }, indx) => (
         <div
           onClick={() => navigate(redirect)}
           className="flex flex-col  items-center px-3 lg:py-2  gap-1 rounded-xl hover:bg-(--bg-tertiary)"
           key={indx}
         >
-          <Icon color={iconsColor} className="h-7 w-7 lg:h-10 h:w-10" />
+          <Icon color={iconsColor} className="h-2 w-2 lg:h-10 h:w-10" />
           <h1 className="text-[10px]  lg:text-[12px] text-(--text-primary)">
             {text}
           </h1>
