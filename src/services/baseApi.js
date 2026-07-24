9f1a8f5ebf115c4e5b3b3b561128205793555bd3
Maaz Jatt <maazcoding54@gmail.com>
2026-07-24 15:37:35 +0500

    added

--- src/services/baseApi.js
+++ src/services/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_REELNEST_BACKEND_URL,
+    baseUrl: "https://reel-nest-backend.vercel.app/api/v1",
     credentials: "include",
   }),
   endpoints: () => ({}),
