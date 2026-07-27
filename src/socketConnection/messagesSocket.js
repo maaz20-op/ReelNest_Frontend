6cbafb6b6977dc0874220e6d55626579e185de23
Maaz Jatt <maazcoding54@gmail.com>
2026-07-28 04:02:29 +0500

    added-final

--- src/socketConnection/messagesSocket.js
+++ src/socketConnection/messagesSocket.js
@@ -1,8 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
-  import.meta.env.VITE_REELNEST_BACKEND_URL_RAILWAY_SERVER,
-  {
-    autoConnect: false,
-  },
-);
+export const socket = io("https://reelnestbackend-production.up.railway.app", {
+  autoConnect: false,
+});
