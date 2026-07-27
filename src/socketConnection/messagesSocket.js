c016a4ddadb4c39cc9445e9f93cf8c31230541df
Maaz Jatt <maazcoding54@gmail.com>
2026-07-28 03:44:53 +0500

    added-final

--- src/socketConnection/messagesSocket.js
+++ src/socketConnection/messagesSocket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
-  autoConnect: false,
-});
+export const socket = io(
+  import.meta.env.VITE_REELNEST_BACKEND_URL_RAILWAY_SERVER,
+  {
+    autoConnect: false,
+  },
+);
