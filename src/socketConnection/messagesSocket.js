60ca310b6e1c4cdd72cf556b2ddc4f5054c54d1a
Maaz Jatt <maazcoding54@gmail.com>
2026-07-28 03:58:24 +0500

    added

--- src/socketConnection/messagesSocket.js
+++ src/socketConnection/messagesSocket.js
@@ -3,7 +3,6 @@ import { io } from "socket.io-client";
 export const socket = io(
   import.meta.env.VITE_REELNEST_BACKEND_URL_RAILWAY_SERVER,
   {
-    transports: ["websocket", "polling"], // Allow both WebSocket and fallback polling
-    withCredentials: true,
+    autoConnect: false,
   },
 );
