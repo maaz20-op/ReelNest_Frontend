062c609664e645a54d95f44e184291a2426d3c18
Maaz Jatt <maazcoding54@gmail.com>
2026-07-28 14:02:56 +0500

    Added

--- src/services/baseApi.js
+++ src/services/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_REELNEST_BACKEND_URL_RAILWAY_SERVER,
+    baseUrl: import.meta.env.VITE_REELNEST_BACKEND_URL_VERCEL_SERVER,
     credentials: "include",
   }),
   endpoints: () => ({}),
--- src/socketConnection/messagesSocket.js
+++ src/socketConnection/messagesSocket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://reelnestbackend-production.up.railway.app", {
+export const socket = io(import.meta.env.VITE_BACKEND_SOCKET_SERVER_VERCEL, {
   autoConnect: false,
 });
