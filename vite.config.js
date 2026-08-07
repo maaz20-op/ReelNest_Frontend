6c42818f4075e367e3a366aecc3828fdd76364f3
Maaz Jatt <maazcoding54@gmail.com>
2026-08-08 01:29:01 +0500

    added

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,6 @@ import { analyzer } from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    minify: true,
-
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // 1. Saari external libraries ko ek alag file mein daalo
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-          // 2. Apne saare components aur pages ko ek single chunk mein merge kar do
-          if (id.includes("src/components/") || id.includes("src/pages/")) {
-            return "components-bundle";
-          }
-        },
-      },
-    },
-  },
   plugins: [
     react(),
     tailwindcss(),
