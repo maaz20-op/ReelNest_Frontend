2d282513933cbeb04e12dee0cf05a05711ec4f22
Maaz Jatt <maazcoding54@gmail.com>
2026-08-13 21:46:06 +0500

    added

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { AppRouting } from "../routes/AppRoutes";
 import { MainLayout } from "../layouts/mainLayout";
 import { ErrorFallback } from "../utils/ErrorBoundary";
 import { ErrorBoundary } from "react-error-boundary";
+import { useAuth } from "../features/auth/hooks/useAuth";
 
 function App() {
+  const { setUser } = useAuth();
   useEffect(() => {
-    const { setUser } = useAuth();
     const queryParams = new URLSearchParams(window.location.search);
 
     const user = queryParams.get("user");
-
+    console.log(user);
     if (user && user?._id) {
       setUser(user);
     }
 
     window.history.replaceState({}, document.title, "/");
   }, []);
+
   return (
     <div className="app ">
       {/* All the Routes of App */}
