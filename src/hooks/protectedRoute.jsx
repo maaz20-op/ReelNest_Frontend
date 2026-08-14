0d38311aa32a1258a77448d14d195d84f60d5167
Maaz Jatt <maazcoding54@gmail.com>
2026-08-14 15:22:13 +0500

    added

--- src/hooks/protectedRoute.jsx
+++ src/hooks/protectedRoute.jsx
@@ -6,7 +6,7 @@ export const ProtectedRoute = () => {
   const location = useLocation();
   const logout = localStorage.getItem("logout");
   const parseLogout = JSON.parse(logout);
-  console.log(user, parseLogout);
+
   if (!user || !user?._id || parseLogout) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
