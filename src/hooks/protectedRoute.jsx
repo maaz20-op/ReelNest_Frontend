63a529468b7170b1abc9f56c2e3f02d7123a5e12
Maaz Jatt <maazcoding54@gmail.com>
2026-08-14 15:45:54 +0500

    added

--- src/hooks/protectedRoute.jsx
+++ src/hooks/protectedRoute.jsx
@@ -4,22 +4,13 @@ import { useAuth } from "../features/auth/hooks/useAuth";
 export const ProtectedRoute = () => {
   const { user, isLoading } = useAuth();
   const location = useLocation();
-
   const logout = localStorage.getItem("logout");
-  const parseLogout = logout ? JSON.parse(logout) : false; // Safe parsing ka tareeqa
-
-  console.log("Loading Status:", isLoading, "User Data:", user);
-
-  // 1. Sabse pehle loading check karein. Jab tak loading hai, yahin rukein.
-  if (isLoading) {
-    return <div>Loading...</div>; // Aap yahan apna koi achha sa Spinner laga sakte hain
-  }
-
-  // 2. Loading khatam hone ke baad check karein ke user logged in hai ya nahi
+  const parseLogout = JSON.parse(logout);
+  console.log(isLoading, user);
   if (!user || !user?._id || parseLogout) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
+    if (!isLoading)
+      return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
-  // 3. Agar loading bhi khatam ho gayi aur user bhi mojud hai, to andar ka page dikhayein
   return <Outlet />;
 };
